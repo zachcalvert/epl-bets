351c79bcc8d50c2a824fa5d8964e3787808cb220
Zach Calvert <zach.calvert@solvhealth.com>
2026-03-14 08:56:13 -0700

    lighter health check, daphne timeouts

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 urlpatterns = [
+    path("healthz", lambda r: HttpResponse("ok"), name="healthz"),
     path("admin/", admin.site.urls),
     path("api/", include("config.api_urls")),
     path("odds/", include("betting.urls")),
